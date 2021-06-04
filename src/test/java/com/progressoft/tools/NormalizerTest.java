38dc5374b65bdb712a5a7188d729724e49d8a15a
mohammad.s.abdellatif@gmail.com <mohammad.s.abdellatif@gmail.com>
2021-06-04 16:34:51 +0300

    chore: add more test cases for Min/Max scalling

--- src/test/java/com/progressoft/tools/NormalizerTest.java
+++ src/test/java/com/progressoft/tools/NormalizerTest.java
@@ -10,6 +10,7 @@ import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public class NormalizerTest {
@@ -26,6 +27,25 @@ public class NormalizerTest {
         if (normalizer == null)
             Assertions.fail("normalizer is not initialized");
         return normalizer;
+    }
+
+    @Test
+    public void givenInvalidInput_whenZscore_thenThrowException() throws IOException {
+        Normalizer normalizer = normalizer();
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.zscore(Paths.get("no_exists"), null, null));
+        Assertions.assertEquals("source file not found", exception.getMessage());
+
+        Path source = copyFile("/marks.csv", Files.createTempFile("marks", ".csv"));
+        exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.zscore(source,
+                        Files.createTempFile("target", ".csv"), "Salary"));
+        Assertions.assertEquals("column Salary not found", exception.getMessage());
+
+        exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.zscore(source,
+                        Files.createTempFile("target", ".csv"), "TESt"));
+        Assertions.assertEquals("column TESt not found", exception.getMessage());
     }
 
     @Test
@@ -83,7 +103,86 @@ public class NormalizerTest {
         Assertions.assertFalse(Files.isDirectory(destPath), "the destination is not a file");
 
         List<String> generatedLines = Files.readAllLines(destPath);
-        Path assertionPath = copyFile("/employees_z.csv", induction.resolve("employees_z.csv"));
+        Path assertionPath = copyFile("/employees_mm.csv", induction.resolve("employees_mm.csv"));
+        List<String> expectedLines = Files.readAllLines(assertionPath);
+        Assertions.assertEquals(expectedLines, generatedLines);
+    }
+
+    @Test
+    public void givenInvalidInput_whenMinMaxScale_thenThrowException() throws IOException {
+        Normalizer normalizer = normalizer();
+        IllegalArgumentException exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.minMaxScaling(Paths.get("no_exists"), null, null));
+        Assertions.assertEquals("source file not found", exception.getMessage());
+
+        Path source = copyFile("/marks.csv", Files.createTempFile("marks", ".csv"));
+        exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.minMaxScaling(source,
+                        Files.createTempFile("target", ".csv"), "Kalven"));
+        Assertions.assertEquals("column Kalven not found", exception.getMessage());
+
+        exception = Assertions.assertThrows(IllegalArgumentException.class,
+                () -> normalizer.minMaxScaling(source,
+                        Files.createTempFile("target", ".csv"), "TESt2"));
+        Assertions.assertEquals("column TESt2 not found", exception.getMessage());
+    }
+
+    @Test
+    public void givenMarksCSVFileToScale_whenMarkColumnIsMinMaxScaled_thenNewCSVFileIsGeneratedWithAdditionalMinMaxScoreColumn() throws IOException {
+        String filename = "marks.csv";
+        Path induction = Files.createTempDirectory("induction");
+        String columnName = "mark";
+        Path csvPath = induction.resolve(filename);
+        Path destPath = induction.resolve("marks_scaled.csv");
+        copyFile("/marks.csv", csvPath);
+        Assertions.assertTrue(Files.exists(csvPath));
+
+        Normalizer normalizer = normalizer();
+        ScoringSummary summary = normalizer.minMaxScaling(csvPath, destPath, columnName);
+        Assertions.assertNotNull(summary, "the returned summary is null");
+
+        Assertions.assertEquals(new BigDecimal("66.00"), summary.mean(), "invalid mean");
+        Assertions.assertEquals(new BigDecimal("16.72"), summary.standardDeviation(), "invalid standard deviation");
+        Assertions.assertEquals(new BigDecimal("285.39"), summary.variance(), "invalid variance");
+        Assertions.assertEquals(new BigDecimal("65.00"), summary.median(), "invalid median");
+        Assertions.assertEquals(new BigDecimal("40.00"), summary.min(), "invalid min value");
+        Assertions.assertEquals(new BigDecimal("95.00"), summary.max(), "invalid maximum value");
+
+        Assertions.assertTrue(Files.exists(destPath), "the destination file does not exists");
+        Assertions.assertFalse(Files.isDirectory(destPath), "the destination is not a file");
+
+        List<String> generatedLines = Files.readAllLines(destPath);
+        Path assertionPath = copyFile("/marks_mm.csv", induction.resolve("marks_mm.csv"));
+        List<String> expectedLines = Files.readAllLines(assertionPath);
+        Assertions.assertEquals(expectedLines, generatedLines);
+    }
+
+    @Test
+    public void givenEmployeesCSVFileToScale_whenSalaryColumnIsMinMaxScaled_thenNewCSVFileIsGeneratedWithAdditionalMinMaxScoreColumn() throws IOException {
+        String filename = "employees.csv";
+        Path induction = Files.createTempDirectory("induction");
+        String columnName = "salary";
+        Path csvPath = induction.resolve(filename);
+        Path destPath = induction.resolve("employees_scaled.csv");
+        copyFile("/employees.csv", csvPath);
+        Assertions.assertTrue(Files.exists(csvPath));
+
+        Normalizer normalizer = normalizer();
+        ScoringSummary summary = normalizer.minMaxScaling(csvPath, destPath, columnName);
+        Assertions.assertNotNull(summary, "the returned summary is null");
+
+        Assertions.assertEquals(new BigDecimal("1701.51"), summary.mean(), "invalid mean");
+        Assertions.assertEquals(new BigDecimal("785.19"), summary.standardDeviation(), "invalid standard deviation");
+        Assertions.assertEquals(new BigDecimal("626466.64"), summary.variance(), "invalid variance");
+        Assertions.assertEquals(new BigDecimal("1758.0"), summary.median(), "invalid median");
+        Assertions.assertEquals(new BigDecimal("299"), summary.min(), "invalid min value");
+        Assertions.assertEquals(new BigDecimal("2965"), summary.max(), "invalid maximum value");
+
+        Assertions.assertTrue(Files.exists(destPath), "the destination file does not exists");
+        Assertions.assertFalse(Files.isDirectory(destPath), "the destination is not a file");
+
+        List<String> generatedLines = Files.readAllLines(destPath);
+        Path assertionPath = copyFile("/employees_mm.csv", induction.resolve("employees_mm.csv"));
         List<String> expectedLines = Files.readAllLines(assertionPath);
         Assertions.assertEquals(expectedLines, generatedLines);
     }
